b966d136a704a12412525f33b57ea4afdb54fb38
RolandMarkarov <roland.m@dewzilla.com>
2020-05-07 16:00:08 +0300

    cleaning logs

--- src/components/Confirm.vue
+++ src/components/Confirm.vue
@@ -22,7 +22,6 @@
         this.$store.commit('openModal', {open: false, confirm: false, action: ''});
       },
       yes() {
-        console.log(this.id, "yes")
         this.$store.commit('openModal', {open: false, confirm: true, action: this.modal.action});
       }
     },
@@ -30,12 +29,10 @@
       modal(next) {
         if (next.confirm) {
           if (next.action === 'delete') {
-            console.log('delete watch')
             this.$store.commit('openModal', {open: false, confirm: false, action: ''});
             return this.$store.commit('deleteTask', this.id)
           }
           if (next.action === 'edit') {
-            console.log('edit watch')
             let temp = {...this.obj, id:this.id}
             this.$store.dispatch('saveEdit', temp)
             this.$store.commit('openModal', {open: false, confirm: false, action: ''});
